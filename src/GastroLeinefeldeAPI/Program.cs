a8f163d40fafe925130ccaceade87e3e41307398
Artem Korenkov <radix.artem@google.com>
2026-08-06 13:18:14 +0200

    Deploy Test DB

--- src/GastroLeinefeldeAPI/Program.cs
+++ src/GastroLeinefeldeAPI/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSwaggerGen();
 
 // Database
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-    ?? "Host=tzz;Database=gastro_menu;Username=postgres;Password=postgres";
+    ?? "Host=postgres;Database=gastro_menu;Username=postgres;Password=postgres";
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
